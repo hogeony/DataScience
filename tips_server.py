886eef2f30d8dcd6e388c651d1a7fbfc2328edf2
Hogeon Seo <hogeony@hogeony.com>
2021-08-14 12:09:06 +0900

    Update tips_server.py

--- tips_server.py
+++ tips_server.py
@@ -29,19 +29,3 @@ r – append or update files or directories to existing archive file.
 # manage permission
 sudo chown <user>:<group> filename
 sudo chown <user>:<group> *
-
-# add, commit, push at once with commit msg: date
-git config --global alias.cmp '!f() { git add --all && git commit -m "`date`" && git push; }; f'
-git cmp
-
-# add, commit, push at once with commit msg input
-git config --global alias.cmp '!f() { git add -A && git commit -m "$@" && git push; }; f'
-git cmp "msg"
-
-# pull, add, commit, push at once with commit msg: date
-git config --global alias.pcmp '!f() { git pull && git add --all && git commit -m "`date`" && git push; }; f'
-git pcmp
-
-# pull, add, commit, push at once with commit msg input
-git config --global alias.pcmp '!f() { git pull && git add -A && git commit -m "$@" && git push; }; f'
-git pcmp "msg"
